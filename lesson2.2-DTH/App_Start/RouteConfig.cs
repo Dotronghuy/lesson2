7298e1852582d8f0a12fbfd0c60866bc3550a423
Dotronghuy <140100028+Dotronghuy@users.noreply.github.com>
2024-04-15 20:22:50 +0700

    add file

---
+++ lesson2.2-DTH/App_Start/RouteConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace lesson2._2_DTH
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Show Product",
+                url: "San-Pham/Danh-Sach-San-Pham",
+                defaults: new { controller = "Product", action = "ShowProduct" }
+            );
+            routes.MapRoute(
+                name: "Product",
+                url: "San-Pham",
+                defaults: new { controller = "Product", action = "ShowProduct" }
+            );
+            routes.MapRoute(
+                name: "Edit Product",
+                url: "San-Pham/Sua/{productId}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "EditProduct",
+                    productId = @"\d{1,4}"
+                }
+            );
+            routes.MapRoute(
+                name: "Details Product",
+                url: "San-Pham/{productName}/{productId}",
+                defaults: new
+                {
+                    controller = "Product",
+                    action = "DetailsProduct",
+                    productName = (string)null,
+                    productId = @"\d{1,4}"
+                }
+            );
+        }
+    }
+}
---
+++ lesson2.2-DTH/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="lesson2._2_DTH.MvcApplication" Language="C#" %>
